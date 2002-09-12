fc5808009c832c7cfb42294732011faebeb3be6a
Kevin Rosenberg <>
2002-09-12 01:21:22 +0000

    Kludge in :version to get CLC LPN to work with CMUCL. This may be a temporary change if a bug in CMUCL's handling of LPN's is found when :version is :newest

--- asdf.lisp
+++ asdf.lisp
@@ -1,4 +1,4 @@
-;;; This is asdf: Another System Definition Facility.  $Revision: 1.38 $
+;;; This is asdf: Another System Definition Facility.  $Revision: 1.39 $
 ;;;
 ;;; The canonical source for asdf is presently the cCLan CVS repository,
 ;;; at <URL:http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/cclan/asdf/>
@@ -86,7 +86,7 @@
 (in-package #:asdf)
 
 ;;; parse the cvs revision into something that might be vaguely useful.  
-(defvar *asdf-revision* (let* ((v "$Revision: 1.38 $")
+(defvar *asdf-revision* (let* ((v "$Revision: 1.39 $")
 			       (colon (position #\: v))
 			       (dot (position #\. v)))
 			  (and v colon dot 
@@ -315,7 +315,10 @@ and NIL NAME and TYPE components"
 			(make-pathname
 			 :name name :case :local :type "asd"
 			 :defaults defaults
-			 :version :newest))))
+			 :version
+			 ;; KMR: Kludge to get CLC LPN's to work with CMUCL
+			 #+common-lisp-controller :unspecific
+			 #-common-lisp-controller :newest))))
 	(if (and file (probe-file file))
 	    (return-from system-definition-pathname file))))
     nil))
