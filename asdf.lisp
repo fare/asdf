46fbe6c99c0eeb627e02cc68417564caa004f153
Daniel Barlow <>
2003-03-16 22:42:54 +0000

    support experimental hyperdoc protocol

--- asdf.lisp
+++ asdf.lisp
@@ -1,4 +1,4 @@
-;;; This is asdf: Another System Definition Facility.  $Revision: 1.63 $
+;;; This is asdf: Another System Definition Facility.  $Revision: 1.64 $
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome: please mail to
 ;;; <cclan-list@lists.sf.net>.  But note first that the canonical
@@ -40,6 +40,7 @@
 (defpackage #:asdf
   (:export #:defsystem #:oos #:operate #:find-system #:run-shell-command
 	   #:system-definition-pathname #:find-component ; miscellaneous
+	   #:hyperdocumentation #:hyperdoc
 	   
 	   #:compile-op #:load-op #:load-source-op #:test-system-version
 	   #:operation			; operations
@@ -87,7 +88,7 @@
 
 (in-package #:asdf)
 
-(defvar *asdf-revision* (let* ((v "$Revision: 1.63 $")
+(defvar *asdf-revision* (let* ((v "$Revision: 1.64 $")
 			       (colon (position #\: v))
 			       (dot (position #\. v)))
 			  (and v colon dot 
@@ -994,6 +995,15 @@ output to *trace-output*.  Returns the shell's exit code."
     #-(or openmcl clisp lispworks allegro scl cmu sbcl)
     (error "RUN-SHELL-PROGRAM not implemented for this Lisp")
     ))
+
+
+(defgeneric hyperdocumentation (package name doc-type))
+(defmethod hyperdocumentation ((package symbol) name doc-type)
+  (hyperdocumentation (find-package package) name doc-type))
+
+(defun hyperdoc (name doc-type)
+  (hyperdocumentation (symbol-package name) name doc-type))
+
 
 (pushnew :asdf *features*)
 
