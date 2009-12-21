f6dfe94d2836534a7be050127123cc16fee87c48
Francois-Rene Rideau <fare@tunes.org>
2009-12-21 03:55:41 -0500

    Releasing 1.374

--- asdf.lisp
+++ asdf.lisp
@@ -163,7 +163,7 @@
 ;;;;
 (defparameter *asdf-revision*
   ;; the 1+ hair is to ensure that we don't do an inadvertent find and replace
-  (subseq "REVISION:1.373" (1+ (length "REVISION"))))
+  (subseq "REVISION:1.374" (1+ (length "REVISION"))))
 
 (defvar *resolve-symlinks* t
   "Determine whether or not ASDF resolves symlinks when defining systems.
