41deb985f1c3c08cc69f36b2d8e9704551557719
Robert P. Goldman <rpgoldman@real-time.com>
2010-02-03 23:22:01 -0500

    Previously, style warnings in compiling asdf.lisp would cause the test script to fail.  Modified to quash style warnings (but not other warnings) during build.
    Signed-off-by: Francois-Rene Rideau <fare@tunes.org>

--- test/compile-asdf.lisp
+++ test/compile-asdf.lisp
@@ -4,15 +4,19 @@
 
 (cond ((probe-file "asdf.lisp")
        (multiple-value-bind (result warnings-p errors-p)
-           (compile-file "asdf.lisp")
+           ;; style warnings shouldn't abort the compilation [2010/02/03:rpg]
+           (handler-bind ((style-warning
+                           #'(lambda (w)
+                               (princ w *error-output*)
+                               (muffle-warning w))))
+             (compile-file "asdf.lisp"))
          (declare (ignore result))
-         (cond (warnings-p 
+         (cond (warnings-p
                 (leave-lisp "Testuite failed: ASDF compiled with warnings" 1))
-               (errors-p 
+               (errors-p
                 (leave-lisp "Testuite failed: ASDF compiled with ERRORS" 2))
                (t
                 (leave-lisp "ASDF compiled cleanly" 0)))))
       (t
        (leave-lisp "Testsuite failed: unable to find ASDF source" 3)))
-       
-                     
+
