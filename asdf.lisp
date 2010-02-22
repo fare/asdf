7a55f292e704df69a0125e291f785526a59122cd
Francois-Rene Rideau <fare@tunes.org>
2010-02-22 10:43:26 -0500

    update ASDF version to 1.623

--- asdf.lisp
+++ asdf.lisp
@@ -208,7 +208,7 @@
   ;; This parameter isn't actually user-visible
   ;; -- please use the exported function ASDF:ASDF-VERSION below.
   ;; the 1+ hair is to ensure that we don't do an inadvertent find and replace
-  (subseq "VERSION:1.622" (1+ (length "VERSION"))))
+  (subseq "VERSION:1.623" (1+ (length "VERSION"))))
 
 (defun asdf-version ()
   "Exported interface to the version of ASDF currently installed. A string.
