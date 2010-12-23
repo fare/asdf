0654a033ea7a8b6adefb7223e12adc2719cba196
Francois-Rene Rideau <fare@tunes.org>
2010-12-23 13:58:43 -0500

    2.011.9: fix an error message (lp#693886). Thanks to Attila Lendvai.

--- asdf.lisp
+++ asdf.lisp
@@ -80,7 +80,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.011.8")
+         (asdf-version "2.011.9")
          (existing-asdf (fboundp 'find-system))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -1681,8 +1681,7 @@ recursive calls to traverse.")
                              required-op required-c required-v))
       (retry ()
         :report (lambda (s)
-                  (format s "~@<Retry loading component ~S.~@:>"
-                          (component-find-path required-c)))
+                  (format s "~@<Retry loading component ~S.~@:>" required-c))
         :test
         (lambda (c)
           (or (null c)
