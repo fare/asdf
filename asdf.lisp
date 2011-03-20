b0564f92f9de671cae1ee1f9deb26eff17d38a97
Francois-Rene Rideau <fare@tunes.org>
2011-03-19 20:14:31 -0400

    2.013: blessing 2.012.12 as our new release. Fix documentation typo and update debian metadata.

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: common-lisp; Base: 10 ; Syntax: ANSI-Common-Lisp -*-
-;;; This is ASDF 2.012.12: Another System Definition Facility.
+;;; This is ASDF 2.013: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -83,7 +83,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.012.12")
+         (asdf-version "2.013")
          (existing-asdf (fboundp 'find-system))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
