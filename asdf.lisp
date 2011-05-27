c0c67dc4191808e5ea1f38f6c5b277e1d8dabb4d
Francois-Rene Rideau <fare@tunes.org>
2011-05-27 13:01:53 -0400

    2.015.7: More tweaking of defaults for Windows thanks to Martin Simmons. Treat compile-file failure cases in decreasing order of importance.

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: common-lisp; Base: 10 ; Syntax: ANSI-Common-Lisp -*-
-;;; This is ASDF 2.015.6: Another System Definition Facility.
+;;; This is ASDF 2.015.7: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -106,7 +106,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.015.6")
+         (asdf-version "2.015.7")
          (existing-asdf (fboundp 'find-system))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -2146,13 +2146,8 @@ recursive calls to traverse.")
     (multiple-value-bind (output warnings-p failure-p)
         (apply *compile-op-compile-file-function* source-file :output-file output-file
                (compile-op-flags operation))
-      (when warnings-p
-        (case (operation-on-warnings operation)
-          (:warn (warn
-                  (compatfmt "~@<COMPILE-FILE warned while performing ~A on ~A.~@:>")
-                  operation c))
-          (:error (error 'compile-warned :component c :operation operation))
-          (:ignore nil)))
+      (unless output
+        (error 'compile-error :component c :operation operation))
       (when failure-p
         (case (operation-on-failure operation)
           (:warn (warn
@@ -2160,8 +2155,13 @@ recursive calls to traverse.")
                   operation c))
           (:error (error 'compile-failed :component c :operation operation))
           (:ignore nil)))
-      (unless output
-        (error 'compile-error :component c :operation operation)))))
+      (when warnings-p
+        (case (operation-on-warnings operation)
+          (:warn (warn
+                  (compatfmt "~@<COMPILE-FILE warned while performing ~A on ~A.~@:>")
+                  operation c))
+          (:error (error 'compile-warned :component c :operation operation))
+          (:ignore nil))))))
 
 (defmethod output-files ((operation compile-op) (c cl-source-file))
   (declare (ignorable operation))
@@ -3884,9 +3884,13 @@ with a different configuration, so the configuration would be re-read then."
                                     "/usr/local/share:/usr/share")
                                 :separator ":"))
             #+asdf-windows
-            ,@`(,(getenv "APPDATA")
-                ,@`(#+lispworks ,(sys:get-folder-path :common-appdata)
-                    ,(try (getenv "ALLUSERSPROFILE") "Application Data/"))))
+            ,@`(,(or #+lispworks (sys:get-folder-path :local-appdata)
+                     (getenv "LOCALAPPDATA"))
+                ,(or #+lispworks (sys:get-folder-path :appdata)
+                     (getenv "APPDATA"))
+                ,(or #+lispworks (sys:get-folder-path :common-appdata)
+                     (getenv "COMMONAPPDATA")
+                     (try (getenv "ALLUSERSPROFILE") "Application Data/"))))
           :collect `(:directory ,(try dir "common-lisp/systems/"))
           :collect `(:tree ,(try dir "common-lisp/source/")))
       :inherit-configuration)))
