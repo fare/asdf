83f86df8cedd278cb3a3519326b017ac4a073b22
Francois-Rene Rideau <tunes@google.com>
2011-12-14 19:04:38 -0500

    2.019.6: Fix to split-absolute-pathnames, by Sergey Katrevich.

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp -*-
-;;; This is ASDF 2.019.5: Another System Definition Facility.
+;;; This is ASDF 2.019.6: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -107,7 +107,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.019.5")
+         (asdf-version "2.019.6")
          (existing-asdf (find-class 'component nil))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -3157,7 +3157,8 @@ located."
                    "Invalid relative pathname ~A~@[ ~?~]" x fmt args))
        x))
 (defun* split-absolute-pathnames (x fmt &rest args)
-  (loop :for dir :in (split-string x :separator (inter-directory-separator))
+  (loop :for dir :in (split-string
+                      x :separator (string (inter-directory-separator)))
     :do (apply 'ensure-absolute-pathname* dir fmt args)
     :collect dir))
 (defun getenv-absolute-pathname (x &aux (s (getenv x)))
