af582a2276f50a8ce971fb8b645276589068c5f8
Francois-Rene Rideau <tunes@google.com>
2012-03-28 13:18:59 -0400

    2.20.5: remove a style-warning on ACL, that hates (or :utf-8 :default) as expanded from the read-conditionals. Use (progn :default :utf-8) instead.

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp -*-
-;;; This is ASDF 2.20.4: Another System Definition Facility.
+;;; This is ASDF 2.20.5: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -112,7 +112,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.20.4")
+         (asdf-version "2.20.5")
          (existing-asdf (find-class 'component nil))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -1293,10 +1293,10 @@ processed in order by OPERATE."))
   new-value)
 
 (defparameter *utf-8-external-format*
-  (or
-   #+(or abcl allegro clozure cmu ecl lispworks (and sbcl sb-unicode) scl) :utf-8
-   #+(and clisp unicode) charset:utf-8
-   :default)
+  (progn ;; (or ... :default) triggers a warning on acl. Sigh.
+    :default
+    #+(or abcl allegro clozure cmu ecl lispworks (and sbcl sb-unicode) scl) :utf-8
+    #+(and clisp unicode) charset:utf-8)
   "Default :external-format argument to pass for CL:OPEN.
 For modern implementations, this should be UTF-8.
 On legacy implementations, we may fall back on some 8-bit encoding,
