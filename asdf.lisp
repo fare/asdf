789948a46f367f82cf420431fffcd3d6dbffda7d
Francois-Rene Rideau <fare@tunes.org>
2012-04-14 17:23:52 -0400

    2.20.13: hostname support for cormanlisp (not really), genera, mcl (untested).

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp -*-
-;;; This is ASDF 2.20.12: Another System Definition Facility.
+;;; This is ASDF 2.20.13: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -115,7 +115,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.20.12")
+         (asdf-version "2.20.13")
          (existing-asdf (find-class 'component nil))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -3213,7 +3213,9 @@ located."
            (or (architecture) (machine-type)))))
 
 (defun* hostname ()
-  #+(or abcl clozure cmucl ecl lispworks sbcl scl xcl) (machine-instance)
+  ;; Note: untested on RMCL
+  #+(or abcl clozure cmucl ecl genera lispworks mcl sbcl scl xcl) (machine-instance)
+  #+cormanlisp "localhost" ;; is there a better way? Does it matter?
   #+allegro (excl.osi:gethostname)
   #+clisp (first (split-string (machine-instance) :separator " "))
   #+gcl (system:gethostname))
