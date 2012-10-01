85d5d45365835f4a4e7c33fc7f8c2f57205304ef
Francois-Rene Rideau <tunes@google.com>
2012-10-01 11:51:15 -0400

    2.25.2: on LispWorks for Windows, enable sys:get-folder-path for personal edition.

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp ; coding: utf-8 -*-
-;;; This is ASDF 2.25.1: Another System Definition Facility.
+;;; This is ASDF 2.25.2: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -118,7 +118,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.25.1")
+         (asdf-version "2.25.2")
          (existing-asdf (find-class 'component nil))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -3352,8 +3352,7 @@ located."
 
 (defun* get-folder-path (folder)
   (or ;; this semi-portably implements a subset of the functionality of lispworks' sys:get-folder-path
-   #+(and lispworks mswindows (not lispworks-personal-edition))
-   (sys:get-folder-path folder)
+   #+(and lispworks mswindows) (sys:get-folder-path folder)
    ;; read-windows-registry HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders\AppData
    (ecase folder
     (:local-appdata (getenv-absolute-directory "LOCALAPPDATA"))
