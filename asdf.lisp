f2a2d29eabf31d679eaaf957736d008985ad7325
Francois-Rene Rideau <tunes@google.com>
2012-12-10 03:05:57 -0500

    2.26.18: Make it easier to name dependencies not in the parent module, by adding a method to find-component that uses the second argument when it's a component, discards the first argument. This makes it easier to build non-canonical dependency graphs.

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp ; coding: utf-8 -*-
-;;; This is ASDF 2.26.17: Another System Definition Facility.
+;;; This is ASDF 2.26.18: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -118,7 +118,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.26.17")
+         (asdf-version "2.26.18")
          (existing-asdf (find-class 'component nil))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -1986,6 +1986,10 @@ PREVIOUS-TIME when not null is the time at which the PREVIOUS system was loaded.
 
 (defmethod find-component ((module module) (name cons))
   (find-component (find-component module (car name)) (cdr name)))
+
+(defmethod find-component ((base component) (actual component))
+  (declare (ignorable base))
+  actual)
 
 
 ;;; component subclasses
