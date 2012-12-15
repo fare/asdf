ed385b2562c53ab76cc5ba282bfdaccd7e7a878b
Francois-Rene Rideau <tunes@google.com>
2012-12-14 22:42:25 -0500

    2.26.23: have force take precedence over force-not, again. Or else force-not :all won't be as useful.

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp ; coding: utf-8 -*-
-;;; This is ASDF 2.26.22: Another System Definition Facility.
+;;; This is ASDF 2.26.23: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -118,7 +118,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.26.22")
+         (asdf-version "2.26.23")
          (existing-asdf (find-class 'component nil))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -2224,7 +2224,8 @@ PREVIOUS-TIME when not null is the time at which the PREVIOUS system was loaded.
 (defun* visit-action (o c plan recurse fun)
   (when (and o c
              (aif (component-if-feature c) (featurep it) t)
-             (not (action-override-p o c 'operation-forced-not)))
+             (or (action-override-p o c 'operation-forced)
+                 (not (action-override-p o c 'operation-forced-not))))
     (visit-dependencies o c recurse)
     (multiple-value-bind (stamp done-p)
         (compute-action-stamp o c :plan plan #|:base-stamp (visit-dependencies o c recurse)|#)
