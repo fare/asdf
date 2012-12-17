5b3b90a24f4e204658469eb5ab66a5fae238b9f3
Francois-Rene Rideau <tunes@google.com>
2012-12-17 10:32:27 -0500

    Fix test on ecl-bytecodes

--- test/test-compile-file-failure.lisp
+++ test/test-compile-file-failure.lisp
@@ -1,6 +1,6 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   ;; CLISP 2.48 has a bug that makes this test fail. Work around:
   #+(or clisp ecl) (when (and (eq asdf:*compile-file-failure-behaviour* :error)
-                              #+ecl (equal (fasl-type) "fasc"))
+                              #+ecl (equal (asdf::fasl-type) "fasc"))
                      (error 'asdf:compile-error :operation "op" :component "comp"))
   (warn "Warning."))
