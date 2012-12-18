2bfe8eaf2ce9e1b69bf692e36968543947963038
Francois-Rene Rideau <tunes@google.com>
2012-12-18 09:47:18 -0500

    2.26.29: fix compiled-file by making it a child-component.

--- asdf.lisp
+++ asdf.lisp
@@ -1,5 +1,5 @@
 ;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp ; coding: utf-8 -*-
-;;; This is ASDF 2.26.28: Another System Definition Facility.
+;;; This is ASDF 2.26.29: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
@@ -118,7 +118,7 @@
          ;; "2.345.6" would be a development version in the official upstream
          ;; "2.345.0.7" would be your seventh local modification of official release 2.345
          ;; "2.345.6.7" would be your seventh local modification of development version 2.345.6
-         (asdf-version "2.26.28")
+         (asdf-version "2.26.29")
          (existing-asdf (find-class 'component nil))
          (existing-version *asdf-version*)
          (already-there (equal asdf-version existing-version)))
@@ -4725,7 +4725,7 @@ with a different configuration, so the configuration would be re-read then."
 ;;; Only useful when the dependencies have also been precompiled.
 ;;;
 
-(defclass compiled-file (component)
+(defclass compiled-file (child-component)
   ((type :initform #-(or ecl mkcl) (fasl-type) #+(or ecl mkcl) "fasb")))
 
 (defmethod trivial-system-p ((s parent-component))
