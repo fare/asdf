df2851618fcf01b48977d2e7ca1cbdc17f07c4cc
Francois-Rene Rideau <tunes@google.com>
2014-01-08 16:16:22 -0500

    Bump version to 3.1.0.45

--- header.lisp
+++ header.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp -*-
-;;; This is ASDF 3.1.0.44: Another System Definition Facility.
+;;; This is ASDF 3.1.0.45: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
