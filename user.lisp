23bacd478f35674b95d5b60856a1302bbff68765
Francois-Rene Rideau <tunes@google.com>
2014-02-22 02:54:02 -0500

    Add UIOP to the use-list of ASDF-USER, after testing in cl-test-grid.

--- user.lisp
+++ user.lisp
@@ -3,6 +3,9 @@
 
 (uiop/package:define-package :asdf/user
   (:nicknames :asdf-user)
-  ;; TODO: it would be nice to have :UIOP in the list,
-  ;; but we need test compatibility with cl-test-grid first.
-  (:use :uiop/common-lisp :uiop/package :asdf/interface))
+  ;; NB: releases before 3.1.1 this :use'd only uiop/package instead of uiop below.
+  ;; They also :use'd uiop/common-lisp, that reexports common-lisp and is not included in uiop.
+  ;; ASDF3 releases from 2.27 to 2.31 called uiop asdf-driver and asdf/foo uiop/foo.
+  ;; ASDF1 and ASDF2 releases (2.26 and earlier) create a temporary package
+  ;; that only :use's :cl and :asdf
+  (:use :uiop/common-lisp :uiop :asdf/interface))
