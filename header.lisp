c943a56e2e7a4e5f4a323af0d59bef7671d1b7b2
Robert P. Goldman <rpgoldman@gmail.com>
2014-05-03 17:22:23 -0500

    Bump version to 3.1.0.120

--- header.lisp
+++ header.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp ; buffer-read-only: t; -*-
-;;; This is ASDF 3.1.0.119: Another System Definition Facility.
+;;; This is ASDF 3.1.0.120: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
