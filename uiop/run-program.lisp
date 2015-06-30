6810657faee4d40cbea108834d6f7e081596f028
Francois-Rene Rideau <tunes@google.com>
2015-06-30 16:00:34 -0400

    UIOP: make run-program/subprocess-error more human-readable
    When a process with a long command fails, it was far too indented to the right.
    Remedy that at the price of making it somewhat uglier when the command is short.

--- uiop/run-program.lisp
+++ uiop/run-program.lisp
@@ -327,7 +327,7 @@ Programmers are encouraged to define their own methods for this generic function
      (command :initform nil :initarg :command :reader subprocess-error-command)
      (process :initform nil :initarg :process :reader subprocess-error-process))
     (:report (lambda (condition stream)
-               (format stream "Subprocess~@[ ~S~]~@[ run with command ~S~] exited with error~@[ code ~D~]"
+               (format stream "Subprocess ~@[~S~% ~]~@[with command ~S~% ~]exited with error~@[ code ~D~]"
                        (subprocess-error-process condition)
                        (subprocess-error-command condition)
                        (subprocess-error-code condition)))))
