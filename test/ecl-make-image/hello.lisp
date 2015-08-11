71f6e79532e3de86b98580036c3bd45096a6f646
Daniel Kochmański <daniel@turtleware.eu>
2015-08-11 09:23:54 +0200

    ecl: add test of ld-flags usage
    Signed-off-by: Daniel Kochmański <daniel@turtleware.eu>

---
+++ test/ecl-make-image/hello.lisp
@@ -0,0 +1,8 @@
+(in-package #:cl-user)
+
+(ffi::clines "extern const char *hello_string;")
+
+(ffi::def-foreign-var ("hello_string" +hello-string+) (* :char) nil)
+
+(print (ffi:convert-from-foreign-string +hello-string+))
+
