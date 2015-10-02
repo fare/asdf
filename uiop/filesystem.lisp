d7a5bc7bdad09876efec3b678fa4e3bbc2dc995a
Francois-Rene Rideau <tunes@google.com>
2015-10-02 15:39:51 -0400

    Enhance uiop:lisp-implementation-directory
    Make it support more platforms.

--- uiop/filesystem.lisp
+++ uiop/filesystem.lisp
@@ -544,15 +544,20 @@ NILs."
   (defun lisp-implementation-directory (&key truename)
     "Where are the system files of the current installation of the CL implementation?"
     (declare (ignorable truename))
-    #+(or clasp clozure ecl gcl mkcl sbcl)
     (let ((dir
             (ignore-errors
+             #+abcl extensions:*lisp-home*
+             #+(or allegro clasp ecl mkcl) #p"SYS:"
+             #+clisp custom:*lib-directory*
              #+clozure #p"ccl:"
-             #+(or clasp ecl mkcl) #p"SYS:"
+             #+cmu (pathname-parent-directory-pathname (truename #p"modules:"))
              #+gcl system::*system-directory*
+             #+lispworks lispworks:*lispworks-directory*
              #+sbcl (if-let (it (find-symbol* :sbcl-homedir-pathname :sb-int nil))
                       (funcall it)
-                      (getenv-pathname "SBCL_HOME" :ensure-directory t)))))
+                      (getenv-pathname "SBCL_HOME" :ensure-directory t))
+             #+scl (pathname-parent-directory-pathname (truename #p"file://modules/"))
+             #+xcl ext:*xcl-home*)))
       (if (and dir truename)
           (truename* dir)
           dir)))
