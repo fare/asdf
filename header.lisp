67fb990596994eb4e9eb4f1e74f30a6b26e3fe3c
Francois-Rene Rideau <tunes@google.com>
2015-11-18 00:03:31 -0500

    Fix header mode line for Genera
    See https://bugs.launchpad.net/asdf/+bug/1517223

--- header.lisp
+++ header.lisp
@@ -1,4 +1,4 @@
-;;; -*- mode: Common-Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp ; buffer-read-only: t; -*-
+;;; -*- mode: Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp ; buffer-read-only: t; -*-
 ;;; This is ASDF 3.1.6.1: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
