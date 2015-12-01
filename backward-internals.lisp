413c0a0d49d2d9237d5c22fe34864549d61d3357
Francois-Rene Rideau <tunes@google.com>
2015-12-01 03:50:54 -0500

    Define backward load-sysdef to unbreak swank-asdf
    Undelete function load-sysdef, which must continue to exist as long as
    people use the current version of SLIME (to be fixed imminently as of
    2015-12-01), though it is OK for it to just issue an error.
    
    Indeed, swank-asdf supposes that any backward-compatibility function it
    defines will exist forever, by unconditionally calling fdefinition on
    them. Now, it depends on load-asd, the backward-compatible definition of
    which (for asdf older than 2.26.149) depends on load-sysdef, that it also
    defines for versions older than 2.16.

--- backward-internals.lisp
+++ backward-internals.lisp
@@ -4,6 +4,11 @@
 (uiop/package:define-package :asdf/backward-internals
   (:recycle :asdf/backward-internals :asdf)
   (:use :uiop/common-lisp :uiop :asdf/upgrade :asdf/find-system)
-  (:export))
+  (:export #:load-sysdef))
 (in-package :asdf/backward-internals)
 
+(with-upgradability ()
+  (defun load-sysdef (name pathname)
+    (declare (ignore name pathname))
+    ;; Needed for backward compatibility with swank-asdf from SLIME 2015-12-01 or older.
+    (error "Use asdf:load-asd instead of asdf::load-sysdef")))
