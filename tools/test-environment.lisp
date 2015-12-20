c421f58201384d49de9ef86ce6f3962f570b3984
Robert P. Goldman <rpgoldman@sift.net>
2015-12-20 15:12:23 -0600

    Suppress empty arguments in results of DEFTESTCMD.

--- tools/test-environment.lisp
+++ tools/test-environment.lisp
@@ -144,7 +144,8 @@ and which systems to test loading with ASDF_TEST_SYSTEMS or s=
         :finally
            (multiple-value-bind (decl body) (decl-and-body decl-and-body)
              (return
-               `(defun ,name (&optional ,@largs)
+               `(defun ,name ,@(if largs `((&optional ,@largs))
+                                 (list nil))
                   ,@decl
                   (with-failure-context (:name ,(command-name name))
                     ,@inits
