176ab3759696a0d45212348358a58952668a04ac
Francois-Rene Rideau <fare@tunes.org>
2016-09-01 20:58:19 -0400

    Fix PREFIX in docstring of WITH-TEMPORARY-FILE
    Thanks to Elias Pipping https://bugs.launchpad.net/asdf/+bug/1604008

--- uiop/stream.lisp
+++ uiop/stream.lisp
@@ -557,7 +557,7 @@ If a string, repeatedly read and evaluate from it, returning the last values."
     "Call a THUNK with stream and/or pathname arguments identifying a temporary file.
 
 The temporary file's pathname will be based on concatenating
-PREFIX (defaults to \"uiop\"), a random alphanumeric string,
+PREFIX (or \"tmp\" if it's NIL), a random alphanumeric string,
 and optional SUFFIX (defaults to \"-tmp\" if a type was provided)
 and TYPE (defaults to \"tmp\", using a dot as separator if not NIL),
 within DIRECTORY (defaulting to the TEMPORARY-DIRECTORY) if the PREFIX isn't absolute.
