612bb059c6399b1cc2a2aef170bc7298ea84aac6
Daniel Kochmański <daniel@turtleware.eu>
2016-09-11 12:20:07 +0200

    Revert "bundle: remove deprecated function bundle-system"
    This reverts commit 6475bce31aa326da99a8b76091760153e2dfdce9.

--- bundle.lisp
+++ bundle.lisp
@@ -328,6 +328,7 @@ for all the linkable object files associated with the system or its dependencies
       ((:program)
        'program-op)))
 
+  ;; DEPRECATED. This is originally from asdf-ecl.lisp. Does anyone use it?
   (defun make-build (system &rest args &key (monolithic nil) (type :fasl)
                              (move-here nil move-here-p)
                              &allow-other-keys)
@@ -354,7 +355,12 @@ for all the linkable object files associated with the system or its dependencies
                         (copy-file f new-f)
                         (delete-file-if-exists f)))
                 :collect new-f)
-          files))))
+          files)))
+
+  ;; DEPRECATED. Does anyone use this?
+  (defun bundle-system (system &rest args &key force (verbose t) version &allow-other-keys)
+    (declare (ignore force verbose version))
+    (apply #'operate 'deliver-asd-op system args)))
 
 ;;;
 ;;; LOAD-BUNDLE-OP
