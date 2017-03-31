4ed76c32050753c8a4450c342a1592881e11d63d
Francois-Rene Rideau <fare@tunes.org>
2017-03-31 17:09:59 -0400

    Add test case for spurious rebuild in ddo
    I noticed that some systems, e.g. fast-io, will cause UIOP, CFFI, and
    everything that depends on it, to be constantly recompiled.
    Interestingly, this doesn't happen with any of the systems that fast-io
    directly depends on: alexandria, trivial-gray-streams, and static-vectors.
    And interestingly, the test case, once reduced, is exactly isomorphic to
    the test case already in test-defsystem-depends-on-phase-overlap.script:
    fast-io => main-system, alexandria => overlapping-dependency,
    static-vectors => intermediate-dependency, bazel => defsystem-dependency
    And indeed, adding a test for it detects the failure.

--- test/defsystem-depends-on/defsystem-dependency.lisp
+++ test/defsystem-depends-on/defsystem-dependency.lisp
@@ -3,3 +3,9 @@
 (incf *dd*)
 (format t "Loaded defsystem-dependency.lisp ~d time~(~:*~p~)~%" *dd*)
 (setf (find-class 'asdf::my-cl-source-file) (find-class 'cl-source-file))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *cdd* 0))
+
+(eval-when (:compile-toplevel :execute)
+  (incf *cdd*))
