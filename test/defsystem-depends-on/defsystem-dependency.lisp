a62ceed581bfbf86680d961dc063ff494fff0006
Francois-Rene Rideau <fare@tunes.org>
2017-10-12 14:32:50 -0400

    Add test for modifying the current syntax in a ddo

--- test/defsystem-depends-on/defsystem-dependency.lisp
+++ test/defsystem-depends-on/defsystem-dependency.lisp
@@ -9,3 +9,17 @@
 
 (eval-when (:compile-toplevel :execute)
   (incf *cdd*))
+
+;; Modify the current *readtable* -- it's very rude, but it's still supported,
+;; and some bugs in previous versions of ASDF (e.g. 3.3.0, fixed in 3.3.0.1)
+;; failed to restore syntax tables inside with-standard-io-syntax, causing it to fail.
+(defun sharp-b (stream chr arg)
+  (declare (ignore chr arg))
+  (let ((arg (read stream nil nil t)))
+    (if *read-suppress*
+        nil
+        (if (stringp arg)
+            (map '(vector (unsigned-byte 8)) 'char-code arg)
+            (error "#b takes a string argument: ~s" arg)))))
+
+(set-dispatch-macro-character #\# #\b #'sharp-b)
