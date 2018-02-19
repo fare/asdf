6cba911f89e15bcde00cced5248190b4d747ab90
Robert P. Goldman <rpgoldman@sift.net>
2018-02-19 13:29:18 -0600

    Bump version to 3.3.1.4

--- uiop/version.lisp
+++ uiop/version.lisp
@@ -12,7 +12,7 @@
 (in-package :uiop/version)
 
 (with-upgradability ()
-  (defparameter *uiop-version* "3.3.1.3")
+  (defparameter *uiop-version* "3.3.1.4")
 
   (defun unparse-version (version-list)
     "From a parsed version (a list of natural numbers), compute the version string"
