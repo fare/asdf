213f108b5b8f0349ea40ca55fa4bc418a4b5a7aa
Francois-Rene Rideau <fare@tunes.org>
2018-08-17 14:44:12 -0400

    Bump version to 3.3.2.7

--- uiop/version.lisp
+++ uiop/version.lisp
@@ -12,7 +12,7 @@
 (in-package :uiop/version)
 
 (with-upgradability ()
-  (defparameter *uiop-version* "3.3.2.6")
+  (defparameter *uiop-version* "3.3.2.7")
 
   (defun unparse-version (version-list)
     "From a parsed version (a list of natural numbers), compute the version string"
