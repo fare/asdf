295d09a03adc7fb64202e44fef70e5d61a5a0448
Robert P. Goldman <rpgoldman@sift.net>
2020-04-29 15:50:25 -0500

    Bump version to 3.3.4.3

--- header.lisp
+++ header.lisp
@@ -1,5 +1,5 @@
 ;;; -*- mode: Lisp; Base: 10 ; Syntax: ANSI-Common-Lisp ; buffer-read-only: t; -*-
-;;; This is ASDF 3.3.4.2: Another System Definition Facility.
+;;; This is ASDF 3.3.4.3: Another System Definition Facility.
 ;;;
 ;;; Feedback, bug reports, and patches are all welcome:
 ;;; please mail to <asdf-devel@common-lisp.net>.
